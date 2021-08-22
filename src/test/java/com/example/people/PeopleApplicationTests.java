3e42cdbe75aaf1bf3cd53a9484fb99232faf2367
ISOLONARI <igor.solonari@orange.md>
2021-08-22 20:32:50 +0300

    search by name

--- src/test/java/com/example/people/PeopleApplicationTests.java
+++ src/test/java/com/example/people/PeopleApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.people;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import com.example.people.controllers.PeopleController;
+import com.example.people.entities.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+
+import java.util.logging.Logger;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PeopleApplicationTests {
 	
+	@Autowired
+	private PeopleController controller;
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+	
+	@Test
+	void getAllPeople() throws Exception {
+		
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/getpeople?page=0&size=10", Object.class)).isNotNull();
 	}
 	
 }
